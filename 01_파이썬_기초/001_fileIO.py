43f63700d5ecab33b63c3ca6f72beef927c0a1cd
domafordarwin <domafordarwin@gmail.com>
2025-02-02 15:29:15 +0900

    환경 변수를 백업하고, 파일 입출력과 경로에 대해 공부했습니다.

---
+++ 01_파이썬_기초/001_fileIO.py
@@ -0,0 +1,13 @@
+import os
+# i = 0
+# print('환경변수 출력하기')
+# for key, value in os.environ.items():
+
+#     if i < 10:
+#         i += 1
+#         print(f'{i}: cl{key} = {value}')
+#     else:
+#         break
+
+env = os.environ
+print(type(env))
